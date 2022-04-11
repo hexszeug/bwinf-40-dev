92d2f1c3a783a731c66512de2b15c358cdcbdf2f
hexszeug <siemen.zielke@gmail.com>
2022-04-11 19:10:25 +0200

    changed sort from distance to average to distance to median

--- 2_Runde/2_Aufgabe/dev/src/__main__.py
+++ 2_Runde/2_Aufgabe/dev/src/__main__.py
@@ -1,52 +1,62 @@
 import random
 from itertools import product
+from statistics import median as _median
 from operations import *
 
 MAX_TRYS = 5
 
 def main(digits):
-    trys = []
-    best_try_diversity = 0
-    for tri in range(MAX_TRYS):
-        numbers = [random.randint(1, 9) for i in range(digits)]
-        results = []
-        operation_lists = []
-        ### GET POSSIBLE SOLUTIONS
-        # find all possible solutions
-        for operations in product(OPERATIONS, repeat=digits - 1):
-            operations = list(operations)
-            try: result = evaluateExpression(numbers, operations)
-            except EvaluationError: continue
-            if result <= 0: continue
-            results.append(result)
-            operation_lists.append(operations)
-        # remove duplicated solutions
-        for i, result in reversed(list(enumerate(results))):
-            if results.count(result) > 1:
-                results.pop(i)
-                operation_lists.pop(i)
-        ### REMOVE UNINTRESTING SOLUTIONS
-        # remove too boring solutions
-        diversitys = [len(set(x)) for x in operation_lists]
-        solutions = list(zip(results, operation_lists, diversitys))
-        best_diversity = max(diversitys)
-        for i, solution in reversed(list(enumerate(solutions))):
-            if solution[2] < best_diversity:
-                solutions.pop(i)
-        del best_diversity
-        # sort list (after their distance of their result to the average)
-        average = sum(results) / len(results)
-        def solutionSorter(solution):
-            return abs(average - solution[0])
-        solutions.sort(key=solutionSorter)
-        del average, solutionSorter
-        trys.append(solutions)
-        if solutions[0][2] > best_try_diversity:
-            best_try_diversity = solutions[0][2]
-    return [x for x in trys if x[0][2] == best_try_diversity][0][0]
+    numbers = [random.randint(1, 9) for i in range(digits)]
+    results = []
+    operation_lists = []
+    def debug():
+        return
+        try: print(len(solutions))
+        except:
+            print(len(list(zip(results, operation_lists))))
+    ### GET POSSIBLE SOLUTIONS
+    # find all possible solutions
+    for operations in product(OPERATIONS, repeat=digits - 1):
+        operations = list(operations)
+        try: result = evaluateExpression(numbers, operations)
+        except EvaluationError: continue
+        if result <= 0: continue
+        results.append(result)
+        operation_lists.append(operations)
+    debug()
+    # remove duplicated solutions
+    for i, result in reversed(list(enumerate(results))):
+        if results.count(result) > 1:
+            results.pop(i)
+            operation_lists.pop(i)
+    debug()
+    ### REMOVE UNINTRESTING SOLUTIONS
+    # remove too boring solutions
+    diversitys = [len(set(x)) for x in operation_lists]
+    solutions = list(zip(results, operation_lists, diversitys))
+    best_diversity = max(diversitys)
+    for i, solution in reversed(list(enumerate(solutions))):
+        if solution[2] < best_diversity:
+            solutions.pop(i)
+    del best_diversity
+    debug()
+    # sort list (after their distance of their result to the average)
+    median = _median(results)
+    def solutionSorter(solution):
+        return abs(median - solution[0])
+    solutions.sort(key=solutionSorter)
+    del median, solutionSorter
+    debug()
+    return solutions[0]
 
-print(main(5))
+digits = 8
+numbers = [random.randint(1, 9) for i in range(digits)]
 
-# for c in range(2, 16):
-#     for i in range(1):
-#         print(c, main(c))
+# for numbers in product(range(1, 10), repeat=digits):
+#     print(main(digits, list(numbers)))
+
+# print(main(digits, numbers))
+
+for c in range(2, 16):
+    for i in range(1):
+        print(c, main(c))
