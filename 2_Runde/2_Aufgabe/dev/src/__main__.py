afa6a8d06cc26a086ad76895065e783fddb5aa1b
hexszeug <siemen.zielke@gmail.com>
2022-04-11 14:41:50 +0200

    fix logic bug causing algo to not iterate over all possible combinations

--- 2_Runde/2_Aufgabe/dev/src/__main__.py
+++ 2_Runde/2_Aufgabe/dev/src/__main__.py
@@ -1,59 +1,52 @@
 import random
-from itertools import combinations_with_replacement
+from itertools import product
 from operations import *
 
 MAX_TRYS = 5
 
 def main(digits):
     trys = []
+    best_try_diversity = 0
     for tri in range(MAX_TRYS):
         numbers = [random.randint(1, 9) for i in range(digits)]
         results = []
         operation_lists = []
-        def debug(): #TODO remove debug function
-            try: print(solutions)
-            except:
-                print(results)
-                print(operation_lists)
         ### GET POSSIBLE SOLUTIONS
         # find all possible solutions
-        for operations in combinations_with_replacement(\
-            [x for x in OPERATIONS],\
-            len(numbers) - 1\
-        ):
+        for operations in product(OPERATIONS, repeat=digits - 1):
             operations = list(operations)
             try: result = evaluateExpression(numbers, operations)
             except EvaluationError: continue
             if result <= 0: continue
             results.append(result)
             operation_lists.append(operations)
-        debug()
         # remove duplicated solutions
         for i, result in reversed(list(enumerate(results))):
             if results.count(result) > 1:
                 results.pop(i)
                 operation_lists.pop(i)
-        debug()
         ### REMOVE UNINTRESTING SOLUTIONS
         # remove too boring solutions
         diversitys = [len(set(x)) for x in operation_lists]
-        max_diversity = max(diversitys)
-        for i in range(len(results) -1, -1, -1):
-            if diversitys[i] < max_diversity:
-                results.pop(i)
-                operation_lists.pop(i)
         solutions = list(zip(results, operation_lists, diversitys))
-        del max_diversity
-        debug()
+        best_diversity = max(diversitys)
+        for i, solution in reversed(list(enumerate(solutions))):
+            if solution[2] < best_diversity:
+                solutions.pop(i)
+        del best_diversity
         # sort list (after their distance of their result to the average)
         average = sum(results) / len(results)
         def solutionSorter(solution):
             return abs(average - solution[0])
         solutions.sort(key=solutionSorter)
         del average, solutionSorter
-        debug()
+        trys.append(solutions)
+        if solutions[0][2] > best_try_diversity:
+            best_try_diversity = solutions[0][2]
+    return [x for x in trys if x[0][2] == best_try_diversity][0][0]
 
-main(5)
+print(main(5))
+
 # for c in range(2, 16):
-#     test = [len(set(main(c, list(numbers))[0][1])) for numbers in combinations_with_replacement(range(1, 10), c)]
-#     print(c, sum(test) / len(test))
+#     for i in range(1):
+#         print(c, main(c))
