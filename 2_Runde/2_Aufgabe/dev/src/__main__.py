7599a41979b752bb2c8733e375740dc4728fd7ce
hexszeug <siemen.zielke@gmail.com>
2022-04-12 11:44:14 +0200

    migrated from self made expression evalueation to python inbuilt

--- 2_Runde/2_Aufgabe/dev/src/__main__.py
+++ 2_Runde/2_Aufgabe/dev/src/__main__.py
@@ -3,7 +3,27 @@ import argparse
 from itertools import product as _product, chain as _chain
 from statistics import median as _median
 
-from operations import *
+#int overloading hack for not allowing divison fraction results
+class DivisionResultNotInteger(Exception): ...
+
+class NoFractionInt(int):
+    def __truediv__(self, __x: int) -> int:
+        __r = super().__truediv__(__x)
+        if __r.is_integer(): return int(__r)
+        raise DivisionResultNotInteger()
+    
+    def __rtruediv__(self, __x: int) -> int:
+        __r = super().__rtruediv__(__x)
+        if __r.is_integer(): return int(__r)
+        raise DivisionResultNotInteger()
+    
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({super().__repr__()})"
+    
+    def __int__(self) -> int:
+        return super().__int__()
+
+OPERATIONS = ["+", "-", "*", "/"]
 
 #parse arguments
 argparser = argparse.ArgumentParser()
@@ -14,15 +34,16 @@ if digits < 2:
     exit("The operation count must not be less than 1")
 
 #generate numbers
-numbers = [random.randint(1, 9) for i in range(digits)]
+numbers = [NoFractionInt(random.randint(1, 9)) for i in range(digits)]
 
 #calculate results for substituting every possible operation combination between "numbers"
 results = []
 operation_lists = []
 for operations in _product(OPERATIONS, repeat=digits - 1):
     operations = list(operations)
-    try: result = evaluateExpression(numbers, operations)
-    except EvaluationError: continue
+    expression = "".join(list(_chain(*list(zip([str(x) for x in numbers], operations))))) + str(numbers[-1])
+    try: result = eval(expression)
+    except DivisionResultNotInteger: continue
     if result <= 0: continue
     results.append(result)
     operation_lists.append(operations)
@@ -56,5 +77,5 @@ output_solution = random.choices(solutions, weights=reversed(list(range(len(solu
 solution_str = f"{''.join(list(_chain(*list(zip([str(x) for x in numbers], output_solution[1])))))}{numbers[-1]}"
 exec(f"calculated_result = int({solution_str})")
 
-print(f"{'?'.join([str(x) for x in numbers])}={output_solution[0]}")
+print(f"{'?'.join([str(int(x)) for x in numbers])}={output_solution[0]}")
 print(f"{solution_str}={calculated_result}")
