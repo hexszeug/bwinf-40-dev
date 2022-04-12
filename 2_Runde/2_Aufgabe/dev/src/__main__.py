e2becbf53a1db8a6eae7c992a5d0371e06a46704
hexszeug <siemen.zielke@gmail.com>
2022-04-12 09:25:38 +0200

    working version (startargs and output)

--- 2_Runde/2_Aufgabe/dev/src/__main__.py
+++ 2_Runde/2_Aufgabe/dev/src/__main__.py
@@ -1,62 +1,60 @@
 import random
-from itertools import product
+import argparse
+from itertools import product as _product, chain as _chain
 from statistics import median as _median
+
 from operations import *
 
-MAX_TRYS = 5
+#parse arguments
+argparser = argparse.ArgumentParser()
+argparser.add_argument("operation_count", type=int, help="count of the operations to guess in the output rizzle")
+args = argparser.parse_args()
+digits = args.operation_count + 1
+if digits < 2:
+    exit("The operation count must not be less than 1")
 
-def main(digits):
-    numbers = [random.randint(1, 9) for i in range(digits)]
-    results = []
-    operation_lists = []
-    def debug():
-        return
-        try: print(len(solutions))
-        except:
-            print(len(list(zip(results, operation_lists))))
-    ### GET POSSIBLE SOLUTIONS
-    # find all possible solutions
-    for operations in product(OPERATIONS, repeat=digits - 1):
-        operations = list(operations)
-        try: result = evaluateExpression(numbers, operations)
-        except EvaluationError: continue
-        if result <= 0: continue
-        results.append(result)
-        operation_lists.append(operations)
-    debug()
-    # remove duplicated solutions
-    for i, result in reversed(list(enumerate(results))):
-        if results.count(result) > 1:
-            results.pop(i)
-            operation_lists.pop(i)
-    debug()
-    ### REMOVE UNINTRESTING SOLUTIONS
-    # remove too boring solutions
-    diversitys = [len(set(x)) for x in operation_lists]
-    solutions = list(zip(results, operation_lists, diversitys))
-    best_diversity = max(diversitys)
-    for i, solution in reversed(list(enumerate(solutions))):
-        if solution[2] < best_diversity:
-            solutions.pop(i)
-    del best_diversity
-    debug()
-    # sort list (after their distance of their result to the average)
-    median = _median(results)
-    def solutionSorter(solution):
-        return abs(median - solution[0])
-    solutions.sort(key=solutionSorter)
-    del median, solutionSorter
-    debug()
-    return solutions[0]
-
-digits = 8
+#generate numbers
 numbers = [random.randint(1, 9) for i in range(digits)]
 
-# for numbers in product(range(1, 10), repeat=digits):
-#     print(main(digits, list(numbers)))
+#calculate results for substituting every possible operation combination between "numbers"
+results = []
+operation_lists = []
+for operations in _product(OPERATIONS, repeat=digits - 1):
+    operations = list(operations)
+    try: result = evaluateExpression(numbers, operations)
+    except EvaluationError: continue
+    if result <= 0: continue
+    results.append(result)
+    operation_lists.append(operations)
 
-# print(main(digits, numbers))
+#remove duplicated solutions
+for i, result in reversed(list(enumerate(results))):
+    if results.count(result) > 1:
+        results.pop(i)
+        operation_lists.pop(i)
 
-for c in range(2, 16):
-    for i in range(1):
-        print(c, main(c))
+#remove too boring solutions
+diversitys = [len(set(x)) for x in operation_lists]
+solutions = list(zip(results, operation_lists, diversitys))
+best_diversity = max(diversitys)
+for i, solution in reversed(list(enumerate(solutions))):
+    if solution[2] < best_diversity:
+        solutions.pop(i)
+del best_diversity
+
+#sort list (after their distance of their result to the median of all results)
+median = _median(results)
+def solutionSorter(solution):
+    return abs(median - solution[0])
+solutions.sort(key=solutionSorter)
+del median, solutionSorter
+
+#output solution #TODO make better
+
+output_solution = random.choices(solutions, weights=reversed(list(range(len(solutions)))))[0]
+
+solution_str = f"{''.join(list(_chain(*list(zip([str(x) for x in numbers], output_solution[1])))))}{numbers[-1]}"
+exec(f"calculated_result = int({solution_str})")
+
+print(f"{'?'.join([str(x) for x in numbers])}={output_solution[0]}")
+print(f"{solution_str}={calculated_result}")
